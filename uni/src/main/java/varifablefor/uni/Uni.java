d3ed83977a0be7b57b7f9018a6ff067b9d24ffec
Cristina <mcholguinsalinas@gmail.com>
2024-09-20 13:20:02 +0200

    create netbeans project

---
+++ uni/src/main/java/varifablefor/uni/Uni.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package varifablefor.uni;
+
+/**
+ *
+ * @author maria
+ */
+public class Uni {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
